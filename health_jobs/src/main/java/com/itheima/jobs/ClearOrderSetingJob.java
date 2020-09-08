699a24ba54353c1bc7564886f8da1ed7a543531f
wangqibo <358882605@qq.com>
2020-09-08 12:09:05 +0800

    第一次提交

--- health_jobs/src/main/java/com/itheima/jobs/ClearOrderSetingJob.java
+++ health_jobs/src/main/java/com/itheima/jobs/ClearOrderSetingJob.java
@@ -2,10 +2,8 @@ package com.itheima.jobs;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.itheima.service.OrderSettingService;
-import org.junit.Test;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -24,20 +22,4 @@ public class ClearOrderSetingJob {
         String format1 = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
         orderSettingService.clearOrderSetingJob(format1);
     }
-
-
-    //@Test
-    /*public void Test01() throws ParseException {
-        Date date = new Date();
-        System.out.println("当前：　"+date);
-
-        long nowTime = date.getTime();
-        System.out.println("当前毫秒：　"+nowTime);
-        long beforeTime = nowTime - (1000L * 60L * 60L * 24L * 30L);
-        System.out.println("30天之前的毫秒：　" + beforeTime);
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String format = simpleDateFormat.format(beforeTime);
-        System.out.println("30天之前的日期：　"+format);
-    }*/
 }
